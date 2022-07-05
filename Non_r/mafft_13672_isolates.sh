7277407f972b117970a4fdbccc52551b2ade527a
bgrdessislava <bgrdessislava@gmail.com>
2022-07-05 07:57:12 +0100

    adding slurm scripts and python code

--- Non_r/mafft_13672_isolates.sh
+++ Non_r/mafft_13672_isolates.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 
-#SBATCH --job-name=mafft_%j
+#SBATCH --job-name=mafft__%j
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=8
 #SBATCH --time=0-1:00:00
-#SBATCH --chdir=/data/zool-microbe-genomics/spet4850/campy_analysis/data/13672_analysis/
+#SBATCH --workdir=/data/zool-microbe-genomics/spet4850/campy_analysis/data/
 #SBATCH --output=/data/zool-microbe-genomics/spet4850/campy_analysis/logs/mafft_%j.out
 #SBATCH --error=/data/zool-microbe-genomics/spet4850/campy_analysis/logs/mafft_%j.err
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=spet4850@spc.ox.ac.uk
 
+module purge
+module load slurm
 module load mafft/7.305
 
 # Get the 'nth' file where n is the array ID
@@ -18,6 +20,4 @@ file=$1
 
 echo Aligning "${file}"
 
-/data/zool-microbe-genomics/spet4850/campy_analysis/code/xmfa2fasta.pl --file "${file}" > "${file/.xmfa/.fasta}"
-
-mafft --thread "${SLURM_CPUS_PER_TASK}" "${file/.xmfa/.fasta}" > "${file/.xmfa/-aligned.fasta}"
+mafft --thread "${SLURM_CPUS_PER_TASK}" "${file}" > "${file/.fasta.gz/-aligned.fasta.gz}"
